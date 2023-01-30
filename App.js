b1044755f1ae8193160ee0880b33d5c494f81a2a
Maxim Rugal <rugal.front@gmail.com>
2023-01-30 17:47:29 +0300

    fix kb

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Bot } from "grammy";
+import { Bot, InputFile } from "grammy";
 import express from "express";
 import cors from "cors";
 
@@ -6,17 +6,14 @@ const token = "5916581638:AAE3EL7MDymbr-aUShiv5FgCv8F3whtx3To";
 const webAppUrl = "https://keepcorn-front-x4vw.vercel.app/";
 const bot = new Bot(token);
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 bot.start();
+
 bot.command("start", async (ctx) => {
-  await ctx.reply("Зарегистрироваться", {
-    reply_markup: {
-      keyboard: [[{ text: "Начать регистрацию", web_app: { url: webAppUrl } }]],
-    },
-  });
-  await ctx.reply("В Keepcorn", {
+  await ctx.replyWithPhoto(new InputFile("./registration.jpg"), {
+    caption: "Зарегистрироваться в Keepcorn",
+    parse_mode: "HTML",
     reply_markup: {
       inline_keyboard: [[{ text: "Начать регистрацию", web_app: { url: webAppUrl } }]],
     },
@@ -26,3 +23,16 @@ bot.command("start", async (ctx) => {
 const PORT = 8000;
 
 app.listen(PORT, () => console.log("server started on PORT " + PORT));
+
+/// OTHER METHODS ///
+
+// await ctx.reply("", {
+//   reply_markup: {
+//     keyboard: [[{ text: "Начать регистрацию", web_app: { url: webAppUrl } }]],
+//   },
+// });
+// await ctx.reply("Зарегистрироваться в Keepcorn", {
+//   reply_markup: {
+//     inline_keyboard: [[{ text: "Начать регистрацию", web_app: { url: webAppUrl } }]],
+//   },
+// });
